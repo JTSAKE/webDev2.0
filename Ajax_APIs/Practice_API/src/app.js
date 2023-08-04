db70c6fb333532010fac83d63bda66c5abf141c5
Jesus Ruelas <jaruelas26@gmailc.com>
2023-08-04 14:59:50 -0600

    Worked more on API practice

--- Ajax_APIs/Practice_API/src/app.js
+++ Ajax_APIs/Practice_API/src/app.js
@@ -1,35 +1,19 @@
 API_ENDPOINT = 'https://6495fed9b08e17c91792ed85.mockapi.io/bill'
-API_Data= [];
-const contentLocation = document.getElementById('contentLocation');
 
-async function getData() {
-    try {
-        const response = await fetch(API_ENDPOINT);
-        API_Data = await response.json();
-    }
-    catch(e){
-        console.log(`Error: ${e}`);
-    }
-}
+fetch(API_ENDPOINT)
+.then(response => response.json())
+.then(data => {
+    // Step 2: Access the DOM element where you want to draw the information
+    const container = document.getElementById('contentLocation');
 
+    // Step 3: Use the map method to generate the HTML elements
+    const htmlElements = data.map(item => {
+        return(`
+            <div id='${item.id}'>${item.name} - ${item.amount} - ${item.dueDate}</div>
+        `);
+    });
 
-async function drawData(data) {
-    try{
-        const htmlElements = data.map (item => {
-            return(
-                `<div class="item${item.id}">
-                <h3>Name: ${item.name}</h3>
-                <h3>Amount: ${item.amount}</h3>
-                <h3>Due Date: ${item.dueDate}</h3>
-                </div>`
-                );
-            });
-            contentLocation.innerHTML = htmlElements.join('');
-        }
-        catch(e) {
-            console.log(e)
-        }
-    }
-    
-getData()
-drawData(API_Data);
+    // Step 4: Append the generated HTML elements to the DOM
+    container.innerHTML = htmlElements.join('');
+})
+.catch(error => console.error('Error fetching data:', error));
